c33cd02b915ab0f21500ad9780bf5e5722e5ad27
darwisyalfarizi <cmxdarwisy32@gmail.com>
2024-04-09 03:13:37 +0700

    add lesson8 loops

---
+++ src/python_basic_fcc/lesson8/loops.py
@@ -0,0 +1,67 @@
+
+value = 1
+# while value <= 10 :
+#     print(value)
+#     value += 1
+
+
+# while value < 10 :
+#     value +=1
+#     if value == 5:
+#         continue
+#     print(value)
+# else :
+#         print("Value is now equal to " + str(value))
+
+
+
+#names  =["Dave", "Sara", "John"]
+
+# for x in names:
+#     print(x)
+
+# it will slice 1 by 1
+# for x in "misisioopei" :
+#     print(x)
+
+
+# for  x in names:
+#     if x == "Sara":
+#         break
+#     print(x)
+
+
+    
+# for  x in names:
+#     if x == "Sara":
+#         continue
+#     print(x)
+
+
+# for x in range(4) :
+#     print(x)
+
+
+# for x in range(1,4) :
+#     print(x)
+
+# for x in range(0, 100, 5):
+#     print(x)
+
+    
+# for x in range(1, 42, 7):
+#     print(x)
+
+
+names = ["Dave" ,"Sara", "John"]
+actions = ["code", "eats", "sleep"]
+
+# for name in names :
+#     for action in actions :
+#         print(name + " :" + action + ".")
+
+
+        
+for action in actions :
+    for name in names :
+        print(name + " :" + action + ".")
